f20cae7cbc4f8ddcf44cd856f74a0b9521d82774
Şeyma Nur Mutlu <33136703+seymanurmutlu@users.noreply.github.com>
2024-08-08 18:06:04 +0300

    Update Fetch-all-EC2-instances.py
    APP&NW Load Balancer N/A issue resolved

--- Fetch-all-EC2-instances.py
+++ Fetch-all-EC2-instances.py
@@ -1,13 +1,8 @@
 import boto3
 import pandas as pd
 
-# Use this code is you are login aws cli with "aws sso login"
-# AWS profiles, should be edited due to profile names in local device 
-profiles = [
-    "profile1", "profile2", "profile3", "profile4", "profile5",
-    "profile6", "profile7", "profile8", "profile9", "profile10",
-    "profile11", "profile12"
-]
+# AWS profilleri
+profiles = [ "DT-Shared-Source-Abdulvehhab-PS-170457940312", "DT-Information-Security-Abdulvehhab-PS-563991483850" ,"CCI-Cloud-Abdulvehhab-PS-076437998716" ]
 
 def get_session(profile_name):
     return boto3.Session(profile_name=profile_name)
@@ -39,6 +34,11 @@ def get_application_load_balancers(session):
     elbv2_client = session.client('elbv2')
     response = elbv2_client.describe_load_balancers()
     return response['LoadBalancers']
+
+def get_target_groups(session):
+    elbv2_client = session.client('elbv2')
+    response = elbv2_client.describe_target_groups()
+    return response['TargetGroups']
 
 data = []
 
@@ -72,14 +72,22 @@ for profile in profiles:
             else:
                 classic_lb_dict[instance_id] = [lb_name]
 
-    # Get Application Load Balancers
+    # Get Application Load Balancers and their target groups
     app_net_lbs = get_application_load_balancers(session)
     app_net_lb_dict = {}
-    lb_arn_to_name = {lb['LoadBalancerArn']: lb['LoadBalancerName'] for lb in app_net_lbs}
     for lb in app_net_lbs:
+        lb_name = lb['LoadBalancerName']
         lb_arn = lb['LoadBalancerArn']
-        lb_name = lb_arn_to_name[lb_arn]
-        app_net_lb_dict[lb_arn] = lb_name
+        elbv2_client = session.client('elbv2')
+        target_groups = elbv2_client.describe_target_groups(LoadBalancerArn=lb_arn)['TargetGroups']
+        for tg in target_groups:
+            targets = elbv2_client.describe_target_health(TargetGroupArn=tg['TargetGroupArn'])['TargetHealthDescriptions']
+            for target in targets:
+                instance_id = target['Target']['Id']
+                if instance_id in app_net_lb_dict:
+                    app_net_lb_dict[instance_id].append(lb_name)
+                else:
+                    app_net_lb_dict[instance_id] = [lb_name]
 
     for instance in instances:
         instance_id = instance.get('InstanceId', 'N/A')
@@ -89,7 +97,7 @@ for profile in profiles:
         enis = [eni['NetworkInterfaceId'] for eni in instance.get('NetworkInterfaces', [])]
         elastic_ips = elastic_ip_dict.get(instance_id, [])
         classic_lbs = classic_lb_dict.get(instance_id, [])
-        app_net_lbs = [app_net_lb_dict.get(eni, 'N/A') for eni in enis]
+        app_net_lbs = app_net_lb_dict.get(instance_id, ['N/A'])
 
         data.append({
             'Instance ID': instance_id,
